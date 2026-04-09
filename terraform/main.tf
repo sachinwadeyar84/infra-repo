094fc05e4aa8a86323151c5972588a91fef92d6e
Sachin84 <Sachinwadeyar84@gmail.com>
2026-04-10 01:33:00 +0530

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,8 @@ provider "azurerm" {
 
 # Resource Group → base container for all resources
 resource "azurerm_resource_group" "rg" {
-  name     = "devops-rg"
-  location = "East US"
+  name = var.resource_group_name
+  location = var.location
 }
 
 # Container Registry → stores Docker images
@@ -19,7 +19,7 @@ resource "azurerm_container_registry" "acr" {
 
 # AKS Cluster → Kubernetes cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "devops-aks"
+  name                = var.aks_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "devopsaks"
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,15 @@
 variable "location" {
   default = "East US"
 }
+
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "devops-rg"
+}
+
+variable "aks_name" {
+  default = "devops-aks"
+}
