b7832670a6ba22e5dbce0bf02244a7f8d93e60b1
sachinwadeyar84 <sachinwadeyar84@gmail.com>
2026-04-10 14:44:21 +0530

    Change VM size from Standard_DS2_v2 to Standard_B2s

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_B2s"
   }
 
   identity {
