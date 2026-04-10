f82052136a5874e645165ab8a630f93b7ac4b134
sachinwadeyar84 <sachinwadeyar84@gmail.com>
2026-04-10 16:43:28 +0530

    Change VM size in default node pool to Standard_DC2s_v3

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 2
-    vm_size    = "Standard_B2s"
+    vm_size    = "Standard_DC2s_v3"
   }
 
   identity {
